a5001ed7d1e11198026e75a8f2b709f52c278d51
BloodyNoah <bloodynoah100@gmail.com>
2022-04-24 20:52:58 +0200

    Worked on the Highway task

--- src/main/java/adris/altoclef/tasks/misc/Highway.java
+++ src/main/java/adris/altoclef/tasks/misc/Highway.java
@@ -4,9 +4,11 @@ import adris.altoclef.AltoClef;
 import adris.altoclef.TaskCatalogue;
 import adris.altoclef.tasks.resources.CollectFoodTask;
 import adris.altoclef.tasksystem.Task;
+import adris.altoclef.trackers.storage.ItemStorageTracker;
 import adris.altoclef.util.helpers.BaritoneHelper;
 import adris.altoclef.util.helpers.StorageHelper;
 import baritone.Baritone;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 
 public class Highway extends Task {
@@ -18,7 +20,9 @@ public class Highway extends Task {
 
     @Override
     protected Task onTick(AltoClef mod) {
-        if (!mod.getItemStorage().hasItem(Items.DIAMOND_LEGGINGS, Items.DIAMOND_BOOTS, Items.DIAMOND_CHESTPLATE, Items.DIAMOND_HELMET, Items.DIAMOND_PICKAXE, Items.DIAMOND)) {
+        int NeededDiamonds = 41;
+        if (mod.getItemStorage().getItemCount(Items.DIAMOND) <= NeededDiamonds && !mod.getItemStorage().hasItem(Items.DIAMOND_CHESTPLATE, Items.DIAMOND_HELMET, Items.DIAMOND_BOOTS, Items.DIAMOND_LEGGINGS)) {
+            setDebugState("Mining for Diamonds!");
             return TaskCatalogue.getItemTask("diamond", 40);
         }
         if (!mod.getItemStorage().hasItem(Items.DIAMOND_CHESTPLATE)) {
@@ -33,10 +37,10 @@ public class Highway extends Task {
         if (!mod.getItemStorage().hasItem(Items.DIAMOND_BOOTS)) {
             return TaskCatalogue.getItemTask("diamond_boots", 1);
         }
-        if (!mod.getItemStorage().hasItem(Items.DIAMOND_PICKAXE)) {
+        if (mod.getItemStorage().getItemCount(Items.DIAMOND_PICKAXE) < 5) {
             return TaskCatalogue.getItemTask("diamond_pickaxe", 5);
         }
-        if (!mod.getItemStorage().hasItem(Items.OBSIDIAN)) {
+        if (mod.getItemStorage().getItemCount(Items.OBSIDIAN) <= 63) {
             return TaskCatalogue.getItemTask("obsidian", 64);
         }
         if (StorageHelper.calculateInventoryFoodScore(mod) < 10) {
